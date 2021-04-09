3a543ad2d91506a8af6ad49259cb95f0c8a52e15
aydanegash <73369360+aydanegash@users.noreply.github.com>
2021-04-09 00:44:10 -0500

    added gitignore fie and basic outline for prompted questions

---
+++ index.js
@@ -0,0 +1,44 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+//Questions for the ReadMe file to be generated
+inquirer.prompt(
+    [
+        {
+            type: 'input',
+            messages: '?',
+            name: 'title',
+        }
+
+        {
+            type: 'input',
+            messages: '?',
+            name: 'title',
+        }
+
+        {
+            type: 'input',
+            messages: '?',
+            name: 'title',
+        }
+
+        {
+            type: 'input',
+            messages: '?',
+            name: 'title',
+        }
+
+        {
+            type: 'input',
+            messages: '?',
+            name: 'title',
+        }
+
+        {
+            type: 'input',
+            messages: '?',
+            name: 'title',
+        }
+    ]
+
+)
